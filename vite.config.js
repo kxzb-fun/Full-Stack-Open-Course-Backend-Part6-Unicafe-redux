e7ee6c1d342efbdb6e23debfdde39a6b3a037784
kxzb.fun <sunday_ever@163.com>
2024-05-30 16:55:26 +0800

    feat: 1. add test file 2. fixed eslint about vitetest

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // https://fullstackopen.com/zh/part5/%E6%B5%8B%E8%AF%95_react_%E5%BA%94%E7%94%A8
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './testSetup.js', 
+  }
 })
